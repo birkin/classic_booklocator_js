8c0d1f206728cb0c30281ce7d13554e27a34b5ff
birkin <birkin.diana@gmail.com>
2015-12-14 14:43:01 -0500

    implements main grab-bib class.

--- clssc_bklctr.js
+++ clssc_bklctr.js
@@ -42,12 +42,8 @@ var clssc_bklctr_handler = new function() {
      * Called by $(document).ready()
      */
     bibnum = "foo";
-    // bibnum = clssc_bklctr_bibfinder.find_bib;
-    update_console( "- bibnum, " + bibnum );
-  }
-
-  var update_console = function( note ) {
-    console.log( "bklctr - " + note );
+    bibnum = clssc_bklctr_bibfinder.grab_bib();  // tries getting the bibnum up to 3 ways
+    console.log( "- lctr; bibnum, " + bibnum );
   }
 
   // var grab_bib = function() {
@@ -102,26 +98,82 @@ var clssc_bklctr_handler = new function() {
 
 var clssc_bklctr_bibfinder = new function() {
 
+  var bibfinder_bibnum = null;
+
   this.grab_bib = function() {
     /* Grabs bib via #recordnum; then continues processing.
      * Called by clssc_bklctr_handler.find_bib
      */
+    console.log( "- lctr; starting clssc_bklctr_bibfinder()" );
     var elmnt = document.querySelector( "#recordnum" );
-    update_console( 'elmnt, ' + elmnt )
+    console.log( '- lctr; elmnt, ' + elmnt )
     if ( elmnt == null ) {
       check_holdings_html_for_bib();
     } else {
       var url_string = elmnt.href;
       var segments = url_string.split( "=" )[1];
-      bibnum = segments.slice( 0,8 );
-      update_console( "- bibnum, " + bibnum );
-      // if ( bibnum == null ) {
-      //   check_holdings_html_for_bib();
-      // } else {
-      //   process_item_table();
-      // }
-    return "bar";
+      bibfinder_bibnum = segments.slice( 0,8 );
+      console.log( "- lctr; bibfinder_bibnum, " + bibfinder_bibnum );
+      if ( bibfinder_bibnum == null ) {
+        check_holdings_html_for_bib();
+      }
     }
+    console.log( "- lctr; returning final bibfinder_bibnum, " + bibfinder_bibnum );
+    return bibfinder_bibnum;
+  }
+
+  var check_holdings_html_for_bib = function() {
+    /* Looks for presence of bib-page link (link may or may not contain bibnum).
+     * Called by grab_bib() if bib is null.
+     */
+    var dvs = document.querySelectorAll(".additionalCopiesNav");  // first of two identical div elements
+    var bibfinder_href_string = null;
+    if ( dvs.length > 0 ) {
+      console.log( "- lctr; in check_holdings_html_for_bib(); checking dvs" );
+      var dv = dvs[0];
+      var el = dv.children[0];  // the div contains a link with the bibnum
+      bibfinder_href_string = el.toString();
+      grab_bib_from_holdings_html( bibfinder_href_string );
+    }
+    console.log( "- lctr; in check_holdings_html_for_bib(); bibfinder_href_string, " + bibfinder_href_string );
+    if ( bibfinder_bibnum == null && bibfinder_href_string != null ) {
+      console.log( "- lctr; in check_holdings_html_for_bib(); no bib luck yet" );
+      grab_bib_from_following_href( bibfinder_href_string );
+    }
+    console.log( "- lctr; end of check_holdings_html_for_bib()" );
+  }
+
+  var grab_bib_from_holdings_html = function( bibfinder_href_string ) {
+    /* Tries to determine bibnum from holdings html; then continues processing.
+     * Called by check_holdings_html_for_bib().
+     */
+    var segment = bibfinder_href_string.split("/")[4];  // eg ".b4069600"
+    if ( segment.length == 9 && segment.slice( 0,2 ) == ".b" ) {
+      bibnum = segment.slice( 1, 9 );  // updates module var
+      console.log( "- lctr; in grab_bib_from_holdings_html(); bibnum, " + bibnum );
+    }
+    console.log( "- lctr; end of grab_bib_from_holdings_html()" );
+  }
+
+  var grab_bib_from_following_href = function( bibfinder_href_string ) {
+    /* Tries to load bib-page and grab bib from permalink element; then continues processing.
+     * Called by check_holdings_html_for_bib()
+     */
+    $.ajaxSetup( {async: false} );  // otherwise processing would immediately continue while $.get() makes it's request asynchronously
+    $.get( bibfinder_href_string, function(data) {
+      var div_temp = document.createElement( "div_temp" );
+      div_temp.innerHTML = data;
+      var nodes = div_temp.querySelectorAll( "#recordnum" );
+      console.log( "- lctr; nodes.length, " + nodes.length );
+      if ( nodes.length > 0 ) {
+        var bib_temp = nodes[0].href.split( "=" )[1];
+        bibfinder_bibnum = bib_temp.slice( 0,8 );  // updates module's var
+        console.log( "- lctr; in grab_bib_from_following_href(); outside of $.get(); bibfinder_bibnum is, " + bibfinder_bibnum );
+      } else {
+        console.log( "- lctr; ah, the tricky multiple results page" );
+      }
+    } );
+    console.log( "- lctr; end of grab_bib_from_following_href()" );
   }
 
 };  // end namespace clssc_bklctr_bibfinder, ```var clssc_bklctr_bibfinder = new function() {```
